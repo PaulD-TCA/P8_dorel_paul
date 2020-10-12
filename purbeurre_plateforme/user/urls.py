2df837508ae0020f51d257b9788316c7752eb78a
Dorel <dorel.paul@gmail.com>
2020-10-12 10:45:34 +0200

    user views last version

--- purbeurre_plateforme/user/urls.py
+++ purbeurre_plateforme/user/urls.py
@@ -3,13 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    # path('', views.tutologin, name='tutologin'),
-    # path('', views.tutologin, name='tutologin'),
     path('login/', views.login_page, name='login_page'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.register_page, name='register_page'),
-    
-    # path('', views.index, name='index'),
-    # path('', views.index, name='results'),
 ]
