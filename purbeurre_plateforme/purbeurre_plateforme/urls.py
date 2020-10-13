fc9c0f89b849849287475e10eaa4b4e67783b55e
Dorel <dorel.paul@gmail.com>
2020-10-13 15:59:31 +0200

    urls fixed

--- purbeurre_plateforme/purbeurre_plateforme/urls.py
+++ purbeurre_plateforme/purbeurre_plateforme/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('search_and_sub/', include('search_and_sub.urls')),
+    path('purbeurre_plateforme.search_and_sub/', include('purbeurre_plateforme.search_and_sub.urls')),
     path('user/', include('user.urls')),
     path('basics_screens/', include('basics_screens.urls')),
     path('', include('search_and_sub.urls')),
