df8a5cbeb5794dfd69462fd54cf221c0f7919237
Dorel <dorel.paul@gmail.com>
2020-10-13 07:05:10 +0200

    basic screens updated

---
+++ purbeurre_plateforme/basics_screens/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BasicsScreensConfig(AppConfig):
+    name = 'basics_screens'
--- purbeurre_plateforme/basics_screens/views.py
+++ purbeurre_plateforme/basics_screens/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+
 # Create your views here.
 def legal_notice(request):
-
+    """Display the legal notice."""
     template = loader.get_template('basics_screens/legal_notice_page.html')
     context = {}
-    # return HttpResponse("Hello, search.")
     return HttpResponse(template.render(context, request))
