f79083fa4522edcc09e66407e613b4847c46c501
Dorel <dorel.paul@gmail.com>
2020-10-13 14:22:38 +0200

    wsgi modified

--- purbeurre_plateforme/purbeurre_plateforme/wsgi.py
+++ purbeurre_plateforme/purbeurre_plateforme/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre_plateforme.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre_plateforme.purbeurre_plateforme.settings')
 
 application = get_wsgi_application()
